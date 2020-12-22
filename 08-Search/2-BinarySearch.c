2e6a69b2dec9bc54ae33c2138d6b1a75b2b29975
Nayeon Kim <inourbubble2@gmail.com>
2020-12-23 05:35:42 +0900

    Update 2-BinarySearch.c

--- 08-Search/2-BinarySearch.c
+++ 08-Search/2-BinarySearch.c
@@ -1,3 +1,4 @@
+// Binary Search with Recursion
 int binarySearch(int key, int low, int high)
 {
 	int middle;
@@ -9,6 +10,21 @@ int binarySearch(int key, int low, int high)
 			return binarySearch(key, low, middle - 1);
 		else 
 			return binarySearch(key, middle + 1, high);
+	}
+	return -1;
+}
+
+// Binary Search with Iteration
+int binarySearch2(int key, int low, int high)
+{
+	int middle;
+	while(low <= high) {
+		middle = (low + high) / 2;
+		if(list[middle] == key) return middle;
+		else if(key > list[middle]) 
+			low = middle + 1;
+		else 
+			high = middle - 1;
 	}
 	return -1;
 }
