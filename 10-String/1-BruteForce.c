0d5824ddeb9326db31a859bd7c90dec78d8f8376
Nayeon Kim <inourbubble2@gmail.com>
2021-01-03 09:00:50 +0900

    Update 1-BruteForce.c

--- 10-String/1-BruteForce.c
+++ 10-String/1-BruteForce.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+
+int bfMatch(char txt[], char pat[], int txtLen, int patLen)
+{
+	int pt = 0;
+	int pp = 0;
+	
+	while(pt != txtLen && pp != patLen) {
+		if(txt[pt] == pat[pp]) {
+			pt++;
+			pp++;
+		} else {
+			pt = pt - pp + 1;
+			pp = 0;
+		}
+	}
+	
+	if(pp == patLen)
+		return pt - pp;
+		
+	return -1;
+}
+
+int main(void)
+{
+	printf("%s에서 %s의 위치는 : %d\n", "abcdefg", "bc", bfMatch("abcdefg", "bc", 7, 2));
+	printf("%s에서 %s의 위치는 : %d\n", "abcdefg", "ef", bfMatch("abcdefg", "ef", 7, 2));
+	printf("%s에서 %s의 위치는 : %d\n", "abcdefg", "k", bfMatch("abcdefg", "k", 7, 1));
+	
+	return 0;
+}
