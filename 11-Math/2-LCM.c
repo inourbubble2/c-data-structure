08fa162034eeddc4cbb04e263fc07492f7587364
Nayeon Kim <inourbubble2@gmail.com>
2021-01-14 00:07:33 +0900

    Update 2-LCM.c

--- 11-Math/2-LCM.c
+++ 11-Math/2-LCM.c
@@ -1,28 +1,6 @@
-#include <stdio.h>
-
-int gcd(int n, int m)
+int lcm(int a, int b)
 {
-	if (m == 0)
-		return n;
-	return gcd(m, n%m);
-}
-
-int lcm(int n, int m)
-{
-	int L, G;
-	G = gcd(n, m);
-	L = (n * m) / G;
-	return L;
-}
-
-int main(void)
-{
-	int n, m;
-	n = 21;
-	m = 28;
-	printf("gcd(%d, %d) = %d\n", n, m, gcd(n, m));
-	printf("lcm(%d, %d) = %d\n", n, m, lcm(n, m));
-	
-	
-	return 0;
+	int g;
+	g = gcd(a, b);
+	return a * b / g; // L = (A * B) / G
 }
